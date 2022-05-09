f422cd8c679ab3d2bf2cb79adc95029fdba3ff28
unknown <amaulanaa995@gmail.com>
2022-05-09 23:28:22 +0700

    created header and footer

---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '../styles/Header.module.scss'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+        <div className={styles.logo}>
+            <Link href='/'>
+                <a>DJ Events</a>
+            </Link>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <Link href='/events'>
+                        <a>Events</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
