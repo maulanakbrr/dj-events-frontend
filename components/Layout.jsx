2a63377a1f932840c57aeb69add4719b2f18a011
unknown <amaulanaa995@gmail.com>
2022-05-10 21:40:08 +0700

    created showcase component and added images on public

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
 import styles from '@/styles/Layout.module.scss'
 
 export default function Layout({title, keywords, description, children}) {
+
+  const router = useRouter()
+
   return (
     <div>
       <Head>
@@ -14,6 +18,10 @@ export default function Layout({title, keywords, description, children}) {
       </Head>
 
       <Header/>
+
+      {
+        router.pathname === '/' && <Showcase/>
+      }
 
       <div className={styles.container}>
         {children}
