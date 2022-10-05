c40965bfd4c45d48ab1fe7bae329bd4fa092c571
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-05 11:10:48 +0700

    updated Pagination component and created pagination module styles

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import LinkWrapper from "./LinkWrapper"
+import styles from '@/styles/Pagination.module.scss'
 
 const Pagination = ({page, last, url}) => {
   return (
-    <div>
+    <div className={styles.pagination}>
       <LinkWrapper href={page > 1 ? `${url}?page=${page -1}` : "#"}>
         <a className={page > 1 ? "btn-secondary" : "btn-secondary btn-disable"}>Previous</a>
       </LinkWrapper>
+
+      <p>{`page ${page} of ${last}`}</p>
 
       <LinkWrapper href={page === last ? "#" : `${url}?page=${page+1}`}>
         <a className={page === last ? "btn-secondary btn-disable" : "btn-secondary"}>Next</a>
