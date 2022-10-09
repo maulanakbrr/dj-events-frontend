31fc93ec65712a87a0a04848e6cd15707a9ce1a1
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-09 22:52:44 +0700

    updated jsconfig, created helpers and parseCookies function, updated DashboardPage

--- pages/account/dashboard.jsx
+++ pages/account/dashboard.jsx
@@ -1,15 +1,39 @@
 import { useContext } from "react"
+import { parseCookies } from "@/helpers/index"
 import Layout from "@/components/Layout"
 import AuthContext from "@/context/AuthContext"
+import { API_URL } from "@/config/index"
 
-const DashboardPage = () => {
+const DashboardPage = ({events}) => {
   const { user } = useContext(AuthContext)
   console.log(user)
+
   return (
     <Layout title="Dashboard">
-      Hi, welcome {user ? user.username : ""} :)
+      <h1>
+        Hi, welcome {user ? user.username : ""} :)
+      </h1>
+      <p>events: {events.length}</p>
     </Layout>
   )
 }
 
 export default DashboardPage
+
+export const getServerSideProps = async ({ req }) => {
+  const { token } = parseCookies(req)
+  const res = await fetch(`${API_URL}/api/events/me`, {
+    method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+  })
+
+  const data = await res.json()
+
+  return {
+    props: {
+      events: data
+    }
+  }
+}
