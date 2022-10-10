8b7d130dcd34e598e5050a3417fbd5c609cd4a16
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-10 07:59:49 +0700

    created DashboardEvent component, Dashboard and DashboardEvents module scss, and updated DashboardPage

--- pages/account/dashboard.jsx
+++ pages/account/dashboard.jsx
@@ -1,19 +1,32 @@
 import { useContext } from "react"
 import { parseCookies } from "@/helpers/index"
 import Layout from "@/components/Layout"
+import DashboardEvent from "@/components/DashboardEvent"
 import AuthContext from "@/context/AuthContext"
 import { API_URL } from "@/config/index"
+import styles from "@/styles/Dashboard.module.scss"
 
 const DashboardPage = ({events}) => {
   const { user } = useContext(AuthContext)
-  console.log(user)
+
+  const handleDelete = id => {
+    console.log(id)
+  }
 
   return (
     <Layout title="Dashboard">
-      <h1>
-        Hi, welcome {user ? user.username : ""} :)
-      </h1>
-      <p>events: {events.length}</p>
+      <div className={styles.dash}>
+        <h1>
+          Hi, welcome {user ? user.username : ""} :)
+        </h1>
+        <h3>My Events</h3>
+
+        {
+          events.map(evt => (
+            <DashboardEvent key={evt.id} evt={evt} handleDelete={handleDelete}/>
+          ))
+        }
+      </div>
     </Layout>
   )
 }
