9f816941dbb06d36fc4b60c78c1ff8c962430500
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-12 17:18:15 +0700

    updated config

--- config/index.js
+++ config/index.js
@@ -1,2 +1,7 @@
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3001'
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
+// DEV PURPOSE
+// export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3001'
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
+
+// PROD
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://dj-events-frontend-a2h6ya0i1-maulanakbrr.vercel.app/'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://dj-events-backend-production-79bb.up.railway.app/'
