4f2fb88238a037869bb049eb76038e3db5148b67
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-12 23:24:48 +0700

    updated NEXT_URL and API_URL on config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
-// DEV PURPOSE
+// // DEV PURPOSE
 // export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3001'
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
 
 // PROD
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://dj-events-frontend-gamma.vercel.app/'
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://dj-events-backend-production-79bb.up.railway.app/'
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://dj-events-frontend-gamma.vercel.app'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://dj-events-backend-production-79bb.up.railway.app'
