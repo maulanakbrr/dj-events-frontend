8630432304390945ac68d80d3d538f65d4badaa7
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-12 17:20:30 +0700

    updated NEXT_URL on config

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
 
 // PROD
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://dj-events-frontend-a2h6ya0i1-maulanakbrr.vercel.app/'
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://dj-events-frontend-gamma.vercel.app/'
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://dj-events-backend-production-79bb.up.railway.app/'
